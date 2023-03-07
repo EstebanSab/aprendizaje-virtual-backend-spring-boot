21b45eb1ed91e7f6d5d6e747efa1638e7d8ef0b9
Esteban <esabinio13@gmail.com>
2023-03-07 10:18:15 -0300

    Campus virtual version 1

--- src/main/java/com/campusVirtual/controller/AdminController.java
+++ src/main/java/com/campusVirtual/controller/AdminController.java
@@ -1,8 +1,5 @@
 package com.campusVirtual.controller;
 
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
